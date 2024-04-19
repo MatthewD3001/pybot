33958889d5617aaf5d5d38bf2172d73873d7035a
Matthew Dunham <mrdunham@mtu.edu>
2024-04-19 01:29:59 -0400

    user functions

--- ButaBot.py
+++ ButaBot.py
@@ -6,6 +6,7 @@ import database as db
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix = '>', intents = intents)
 
@@ -18,27 +19,106 @@ async def on_ready():
     db.init_db()
 
 @bot.command()
-async def letter(ctx, *, msg: str=''):
-    await ctx.send(f'Writing letter for {ctx.author.id} which contains: {msg}')
-    user_id = ctx.author.id
-    if db.in_db(user_id):
-        letter = db.get_letter(user_id)
-        if len(letter) > 0:
-            await ctx.send(f'Your letter:\n\n{letter}')
+async def join(ctx):
+    user = ctx.author
+    if db.in_db(user.id):
+        if db.get_in_swap(user.id):
+            await ctx.send(f'You\'re already in swap, {user.name}!')
+        elif db.is_banned(user.id):
+            await ctx.send(f'Sorry, you\'ve been banned, {user.name} :(')
         else:
-            await ctx.send('Edit letter')
+            db.set_in_swap(user.id, 1)
+            await ctx.send(f'Welcome back, {user.name}!')
     else:
-        db.add_user(user_id, msg)
+        db.add_user(user.id)
+        await ctx.send(f'Thanks for joining swap, {user.name}!')
 
-    db.list_users()
 
+@bot.command()
+async def leave(ctx):
+    user = ctx.author
+    if db.in_db(user.id):
+        if db.is_banned(user.id):
+            await ctx.send(f'You\'ve already left swap due to being banned.')
+        else:
+            db.set_in_swap(user.id, 0)
+            await ctx.send(f'You have left the swap, we\'ll miss you, {user.name}!')
+    else:
+        await ctx.send(f'You\'re not in swap! You should reconsider and join instead, {user.name}!')
+
+@bot.command()
+async def letter(ctx, *, msg: str=''):
+    user = ctx.author
+    if db.in_db(user.id):
+        letter = db.get_letter(user.id)
+        if len(msg) < 1:
+            if len(letter) > 0:
+                await ctx.send(f'Your letter:\n{letter}')
+            else:
+                await ctx.send('You do not have a letter yet.')
+        else:
+            db.edit_letter(user.id, msg)
+            letter = db.get_letter(user.id)
+            await ctx.send(f'Your letter:\n{letter}')
+    else:
+        await ctx.send('You\'re not in the swap yet, do `>join` to join!')
+
+
+@bot.command()
+async def ryuu(ctx):
+    await ctx.send(':umu:')
+
+
+#############################
+#                           #
+#       ADMIN COMMANDS      #
+#                           #
+#############################
 @bot.command()
 async def shutdown(ctx):
     if ctx.author.id == 369982786356117504:
         await ctx.send('Shutting down...')
+        db.close_db()
         await bot.close()
     else:
         await ctx.send('You\'re no admin!! :bonk:')
+
+
+@bot.command()
+async def ban(ctx, user_id: int):
+    if ctx.author.id == 369982786356117504:
+        db.set_ban(user_id, 1)
+        await ctx.send(f'Banned user: {user_id}')
+    else:
+        await ctx.send('You\'re no admin!! :bonk:')
+
+
+@bot.command()
+async def start(ctx):
+    if ctx.author.id == 369982786356117504:
+        db.start_swap()
+    else:
+        await ctx.send('You\'re no admin!! :bonk:')
+
+
+@bot.command()
+async def list_swap(ctx):
+    if ctx.author.id == 369982786356117504:
+        giftee_ids = db.get_user_ids_in_swap()
+        output = "## Users in this swap:\n"
+        cur_id = giftee_ids[0]
+        for i in range(len(giftee_ids)):
+            user = bot.get_user(cur_id)
+            if user != None:
+                output += str(user.name) + " -> "
+            else:
+                output += str(cur_id) + " -> "
+            cur_id = db.get_giftee(cur_id)
+        output += str(giftee_ids[0])
+        await ctx.send(output)
+    else:
+        await ctx.send('You\'re no admin!! :bonk:')
+
 
 file = open("../../pass/bottoken.txt", "r")
 token = file.read()
--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 import sqlite3
+import random
 
 def init_db():
+    global conn
+    global cur
     conn = sqlite3.connect('data/users/swap.db')
     cur = conn.cursor()
 
@@ -14,102 +17,183 @@ def init_db():
             user_id int,
             letter text,
             submit text,
+            in_swap int,
             banned int,
-            santa_id int,
+            giftee_id int,
             PRIMARY KEY(user_id)
         );
     """)
     conn.commit()
+    
+    add_user(477096301364903936, "toby's letter")
+    add_user(105365884951863296, "noon's letter")
 
-    cur.execute(f"""
-        INSERT INTO users VALUES (
-            123,
-            'example letter',
-            '',
-            0,
-            0
-        );
-    """)
-    conn.commit()
-
-    cur.execute(f"""
-        INSERT INTO users VALUES (
-            321,
-            'example letter',
-            '',
-            0,
-            0
-        );
-    """)
-    conn.commit()
+def close_db():
     conn.close()
 
-def add_user(user_id: int, letter: str):
-    conn = sqlite3.connect('data/users/swap.db')
-    cur = conn.cursor()
 
+def get_user_ids_in_swap() -> list:
+    cur.execute("""
+        SELECT user_id
+        FROM users
+        WHERE in_swap = 1
+    """)
+    user_ids = cur.fetchall()
+    for index in range(len(user_ids)):
+        user_ids[index] = user_ids[index][0]
+    return user_ids
+
+def get_user_id_in_swap() -> int:
+    cur.execute("""
+        SELECT user_id
+        FROM users
+        WHERE in_swap = 1
+        LIMIT 1
+    """)
+    return cur.fetchone()[0]
+
+
+def start_swap():
+    santa_ids = get_user_ids_in_swap()
+    random.shuffle(santa_ids)
+    for index in range(len(santa_ids) - 1):
+        set_giftee(santa_ids[index], santa_ids[index + 1])
+    set_giftee(santa_ids[len(santa_ids) - 1], santa_ids[0])
+
+    list_users()
+
+
+def get_giftee(santa_id: int):    
+    cur.execute(f"""
+        SELECT giftee_id
+        FROM users
+        WHERE user_id = {santa_id}
+    """)
+    return cur.fetchone()[0]
+
+
+def set_giftee(user_id: int, giftee_id: int):
+    cur.execute(f"""
+        UPDATE users
+        SET giftee_id = {giftee_id}
+        WHERE user_id = {user_id}
+    """)
+    conn.commit()
+
+
+def add_user(user_id: int, letter: str=''):
     cur.execute(f"""
         INSERT INTO users VALUES (
             {user_id},
-            '{letter}',
+            "{letter}",
             '',
+            1,
             0,
             0
         );
     """)
-
     conn.commit()
-    conn.close()
+    list_users()    
+
+def remove_user(user_id: int):
+    cur.execute(f"""
+        DELETE FROM users
+        WHERE user_id = {user_id}
+    """)
+    conn.commit()
+    list_users()    
+
+
+def get_in_swap(user_id: int) -> bool:
+    cur.execute(f"""
+        SELECT in_swap
+        FROM users
+        WHERE user_id = {user_id}
+    """)
+    result = cur.fetchone()[0]
+    print(f'result: {result}')
+    if result == 1:
+        return True
+    else:
+        return False
+
+
+def set_in_swap(user_id: int, in_swap: int):
+    cur.execute(f"""
+        UPDATE users
+        SET in_swap = {in_swap}
+        WHERE user_id = {user_id}
+    """)
+    conn.commit()
+
+def set_ban(user_id: int, ban_status: int):
+    cur.execute(f"""
+        UPDATE users
+        SET banned = {ban_status}
+        WHERE user_id = {user_id}
+    """)
+    conn.commit()
+    if ban_status == 1:
+        set_in_swap(user_id, 0)
+    list_users()    
+
 
 def in_db(user_id: int) -> bool:
-    conn = sqlite3.connect('data/users/swap.db')
-    cur = conn.cursor()
-
     cur.execute(f"""
         SELECT rowid
         FROM users
         WHERE user_id = {user_id}
     """)
-    result = cur.fetchall()
-    if len(result) != 0:
+    result = cur.fetchone()
+    if result != None:
         return True
     else:
         return False
 
-    conn.commit()
-    conn.close()
+
+def is_banned(user_id: int) -> bool:
+    cur.execute(f"""
+        SELECT banned
+        FROM users
+        WHERE user_id = {user_id}
+    """)
+    result = cur.fetchone()[0]
+    if result == 1:
+        return True
+    else:
+        return False
+
 
 def get_letter(user_id: int) -> str:
-    conn = sqlite3.connect('data/users/swap.db')
-    cur = conn.cursor()
-
     cur.execute(f"""
         SELECT letter
         FROM users
         WHERE user_id = {user_id}
     """)
-    letter = str(cur.fetchone())
-    letter = letter[2 : len(letter) - 3]
-    print(f'\n\n{letter}\n\n')
+    letter = cur.fetchone()[0]
+    #letter = letter[2 : len(letter) - 3]
 
-    conn.commit()
-    conn.close()
     return letter
 
-def list_users():
-    conn = sqlite3.connect('data/users/swap.db')
-    cur = conn.cursor()
+def edit_letter(user_id: int, letter: str):
+    cur.execute(f"""
+        UPDATE users
+        SET letter = "{letter}"
+        WHERE user_id = {user_id}
+    """)
+    conn.commit()
+    list_users()    
 
+def list_users():
     cur.execute("""
         SELECT * FROM users; 
     """)
     
     items = cur.fetchall()
+    print()
     for item in items:
-        print(item)
+        print(item) 
 
-    conn.commit()
-    conn.close()
 
 #init_db()
 #add_user(123456789, 'bananas')
